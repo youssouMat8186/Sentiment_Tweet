5df8e8eaecea2e85a5bd5d9fc91dacf73a9e7a7b
youssouMat8186 <90332740+youssouMat8186@users.noreply.github.com>
2023-07-18 14:21:01 -0400

    Update script4.py

--- script4.py
+++ script4.py
@@ -1,9 +1,25 @@
 import pandas as pd
 import streamlit as st
+
+def find_tweets_with_text(text, sentiment, type_produit, train_data):
+    # Convertir le texte recherché en minuscules pour une recherche insensible à la casse
+    text = text.lower()
+
+    # Filtrer les tweets contenant le texte spécifié (insensible à la casse)
+    matching_tweets = train_data[train_data['Product_Description'].str.lower().str.contains(text)]
+
+    # Filtrer les tweets en fonction du sentiment
+    if sentiment != -1:
+        matching_tweets = matching_tweets[matching_tweets['Sentiment'] == sentiment]
+
+    # Filtrer les tweets en fonction du type de produit
+    if type_produit != "Tout":
+        matching_tweets = matching_tweets[matching_tweets['Product_Type'] == type_produit]
+
+    # Renvoyer les tweets correspondants
+    return matching_tweets['Product_Description'].tolist()
+
 def Tweet_Mots_Cles(train_data):
-    # Chargement des données
-    train_data = pd.read_csv('Train.csv', delimiter=',')
-    
     # Titre de l'application
     st.subheader("Recherche de tweets par mot clé")
 
@@ -23,28 +39,12 @@ def Tweet_Mots_Cles(train_data):
     # Nombre de tweets à afficher
     nombre_tweets_affichage = st.slider("Nombre de tweets à afficher", min_value=1, max_value=100, value=10, step=1)
 
-    # Définir la fonction find_tweets_with_text
-    def find_tweets_with_text(text):
-        # Filtrer les tweets contenant le texte spécifié
-        matching_tweets = train_data[train_data['Product_Description'].str.contains(text)]
-        
-        # Filtrer les tweets en fonction du sentiment
-        if sentiment != -1:
-            matching_tweets = matching_tweets[matching_tweets['Sentiment'] == sentiment]
-        
-        # Filtrer les tweets en fonction du type de produit
-        if type_produit != "Tous les produits":
-            matching_tweets = matching_tweets[matching_tweets['Product_Type'] == type_produit]
-        
-        # Renvoyer les tweets correspondants
-        return matching_tweets['Product_Description'].tolist()
-
     # Bouton de recherche
     if st.button("Rechercher"):
         # Vérifier si un texte de recherche a été saisi
         if search_text:
             # Appeler la fonction find_tweets_with_text pour trouver les tweets correspondants
-            matching_tweets = find_tweets_with_text(search_text)
+            matching_tweets = find_tweets_with_text(search_text, sentiment, type_produit, train_data)
 
             # Filtrer le nombre de tweets à afficher
             tweets_affichage = matching_tweets[:nombre_tweets_affichage]
@@ -58,20 +58,4 @@ def Tweet_Mots_Cles(train_data):
                 st.warning("Aucun tweet correspondant trouvé.")
         else:
             st.warning("Veuillez saisir un texte à rechercher.")
-    
-    # Résumé de l'analyse
-    st.markdown("""
-    Cet outil nous permet de rechercher des tweets dans l'ensemble des données d'entraînement en fonction d'un mot présent dans le tweet, du type de produit et du sentiment. Dans cette section, nous nous concentrons sur les tweets négatifs.
 
-    En analysant la répartition des tweets par sentiments et par produits, nous avons constaté que la majorité des tweets négatifs sont liés au type de produit 6. En examinant de plus près le nuage de mots, nous avons remarqué que les termes "Design Headaches", "iPad Design" et "tapworthy" sont fréquemment mentionnés dans les tweets négatifs associés au type de produit 6, mais ils sont rares dans l'ensemble des données.
-
-    Nous avons donc utilisé notre outil de recherche de mots-clés pour analyser les tweets négatifs contenant ces termes. Cette analyse a révélé une tendance générale de moqueries et de critiques envers le design de l'iPad 2 lors du festival SXSW.
-
-    Les utilisateurs expriment leur mécontentement envers le design de l'iPad, en mentionnant des problèmes de conception, des difficultés d'utilisation et des critiques sur l'esthétique de l'appareil.
-
-    Ces résultats sont basés sur l'analyse des tweets et des opinions exprimées par les utilisateurs. Il est important de noter que les problèmes spécifiques liés au design de l'iPad 2 peuvent varier d'un utilisateur à l'autre, mais des problèmes d'ergonomie, de fragilité, de poids, d'esthétique et d'innovation ont été fréquemment mentionnés.
-                
-    Ces observations nous aident à mieux comprendre les réactions des utilisateurs face au design de l'iPad 2 et peuvent être utiles pour identifier les aspects à améliorer dans les futurs développements de produits.
-    
-    En résumé, notre démarche pour l'analyse des sentiments des tweets est la suivante : nous avons examiné les différents types de produits afin de déterminer ceux qui suscitent le plus de tweets négatifs. À l'aide d'un nuage de mots, nous avons identifié les mots les plus fréquents dans les tweets négatifs pour ces types de produits. Ensuite, nous avons analysé spécifiquement les tweets négatifs contenant ces mots clés importants.
-    """)
